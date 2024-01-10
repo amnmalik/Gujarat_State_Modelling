70d835fa0391f7bf02473ecb4e30d999865c64d1
Nikita Shukla <nikita.shukla@ceew.in>
2024-01-10 15:12:22 +0530

    Updated socioeconomics of Gujarat

--- GCAM_State/input/gcamdata/R/zchunk_data_Maddison_population.R
+++ GCAM_State/input/gcamdata/R/zchunk_data_Maddison_population.R
@@ -15,9 +15,7 @@
 #' @import dplyr
 #' @importFrom tidyr gather spread
 #' @author BBL
-
 module_data_Maddison_population <- function(command, ...) {
-  library (dplyr)
   if(command == driver.DECLARE_INPUTS) {
     return(NULL)
   } else if(command == driver.DECLARE_OUTPUTS) {
